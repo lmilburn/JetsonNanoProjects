2fa5000dac1eaac627d2f8fff7a0c35af3756f50
lmilburn <66505573+lmilburn@users.noreply.github.com>
2021-01-02 13:57:00 -0800

    Hello AI World
    added comments

--- my-recognition-python/my-recognition.py
+++ my-recognition-python/my-recognition.py
@@ -5,17 +5,22 @@ import jetson.utils
 
 import argparse
 
+#usage: my-recognition.py [-h] [--network NETWORK] filename
+
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", type=str, help="filename of image to process")
 parser.add_argument("--network", type=str, default="googlenet", help="model to use, can be: googlenet, resnet-18, ect., see --help for others")
 opt = parser.parse_args()
 
+#load the image and specify the model to be used
 img = jetson.utils.loadImage(opt.filename)
 
 net = jetson.inference.imageNet(opt.network)
 
+#Classify() index and floating-point returns are assigned, respectively
 class_idx, confidence = net.Classify(img)
 
+#Using the returned index, find the appropriate class desc
 class_desc = net.GetClassDesc(class_idx)
 
 print("Image is recognized as '{:s}' (class #{:d}) with {:f}% confidence".format(class_desc, class_idx, confidence*100))
